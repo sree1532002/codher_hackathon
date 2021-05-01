14ecdf085d19cb4975e1ffc9b6940c2bac512cec
akshayar0811 <akshayar0811@gmail.com>
2021-05-02 03:53:29 +0530

    Text for astro club

--- club1.php
+++ club1.php
@@ -44,22 +44,7 @@
         </ul>
       </div>
   </div>
-  <!--<div class = "welcome">Welcome to the Astronomy Club!</div>
-  <div class="container-fluid grid">
-  <div class="row">
-    <div class="col-md-6 gridimg">
-    </div>
-    <div class="col-md-6">
-      One of two columns
-    </div>
-   </div>
-  </div> 
-  <div class="form-group">
-  <label for="Text">Text:</label>
-  <textarea class="form-control" rows="5" id="comment"></textarea>
-  </div>
-  <div class = "logbtn col-md-12 text-center"><button type="submit" class="btn btn-primary" name = "login"><a href="">Link</a></button></div>
- --> 
+  <div class = "welcome">Welcome to the Astronomy Club!</div>
   <div class="card mb-3" style="max-width: 100%;">
   <div class="row no-gutters">
     <div class="col-md-4">
@@ -67,12 +52,13 @@
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">Get to know us!</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <div class = "ideas">
+          <div class = "card-title">We believe you've got great ideas, Share em with us.</div>
+        </div>
       </div>
     </div>
   </div>
-</div>
   </body>
   </html>
