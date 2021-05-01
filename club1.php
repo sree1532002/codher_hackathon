95dccae1d685a1c25b23ef69de7869948f52bed0
akshayar0811 <akshayar0811@gmail.com>
2021-05-02 02:54:53 +0530

    Club 1 page

--- club1.php
+++ club1.php
@@ -16,13 +16,48 @@
     <title>Login</title>
 
     <!--CSS Style-->
-    <link rel = "stylesheet" href = "styling.css">
+    <link rel = "stylesheet" href = "clubs.css">
   </head>
   <body>
-    <div class="form-group">
-    <label for="Text">Text:</label>
-    <textarea class="form-control" rows="5" id="comment"></textarea>
+  <div class="bg-img" class="container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#b8b5ff;">
+      <div id = "p" class="float-child">
+        <ul class="nav justify-content-end">
+        <li class="nav-item" >
+            <a class="nav-link"  id = "item" href="#about">Home</a>
+          </li>
+          <li class="nav-item" >
+            <a class="nav-link"  id = "item" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id = "item" href="gallery.html">Clubs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id = "item" href="blog.html">Announcements</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id = "item" href="#contact">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id = "item" href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+  </div>
+  <div class = "welcome">Welcome to the Astronomy Club!</div>
+  <div class="container-fluid grid">
+  <div class="row">
+    <div class="col-md-6 gridimg">
     </div>
-    <div class = "logbtn col-md-12 text-center"><button type="submit" class="btn btn-primary" name = "login"><a href="">Link</a></button></div>
+    <div class="col-md-6">
+      One of two columns
+    </div>
+   </div>
+  </div> 
+  <div class="form-group">
+  <label for="Text">Text:</label>
+  <textarea class="form-control" rows="5" id="comment"></textarea>
+  </div>
+  <div class = "logbtn col-md-12 text-center"><button type="submit" class="btn btn-primary" name = "login"><a href="">Link</a></button></div>
   </body>
   </html>
